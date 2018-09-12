ade97390b443203f39f51eaeb2bbce7bdbee8cf1
Manjunath <manjumjng2@gmail.com>
2018-09-12 20:32:29 +0530

    Fixed Stylesheets and JS files path

--- includes/footer.php
+++ includes/footer.php
@@ -17,48 +17,48 @@
 	<!-- JavaScript -->
 	
     <!-- jQuery -->
-    <script src="admin_assets/vendors/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="admin/admin_assets/vendors/bower_components/jquery/dist/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="admin_assets/vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="admin/admin_assets/vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     
 	<!-- Data table JavaScript -->
-	<script src="admin_assets/vendors/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+	<script src="admin/admin_assets/vendors/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
 	
 	<!-- Slimscroll JavaScript -->
-	<script src="admin_assets/dist/js/jquery.slimscroll.js"></script>
+	<script src="admin/admin_assets/dist/js/jquery.slimscroll.js"></script>
 	
 	<!-- simpleWeather JavaScript -->
-	<script src="admin_assets/vendors/bower_components/moment/min/moment.min.js"></script>
-	<script src="admin_assets/vendors/bower_components/simpleWeather/jquery.simpleWeather.min.js"></script>
-	<script src="admin_assets/dist/js/simpleweather-data.js"></script>
+	<script src="admin/admin_assets/vendors/bower_components/moment/min/moment.min.js"></script>
+	<script src="admin/admin_assets/vendors/bower_components/simpleWeather/jquery.simpleWeather.min.js"></script>
+	<script src="admin/admin_assets/dist/js/simpleweather-data.js"></script>
 	
 	<!-- Progressbar Animation JavaScript -->
-	<script src="admin_assets/vendors/bower_components/waypoints/lib/jquery.waypoints.min.js"></script>
-	<script src="admin_assets/vendors/bower_components/jquery.counterup/jquery.counterup.min.js"></script>
+	<script src="admin/admin_assets/vendors/bower_components/waypoints/lib/jquery.waypoints.min.js"></script>
+	<script src="admin/admin_assets/vendors/bower_components/jquery.counterup/jquery.counterup.min.js"></script>
 	
 	<!-- Fancy Dropdown JS -->
-	<script src="admin_assets/dist/js/dropdown-bootstrap-extended.js"></script>
+	<script src="admin/admin_assets/dist/js/dropdown-bootstrap-extended.js"></script>
 	
 	<!-- Sparkline JavaScript -->
-	<script src="admin_assets/vendors/jquery.sparkline/dist/jquery.sparkline.min.js"></script>
+	<script src="admin/admin_assets/vendors/jquery.sparkline/dist/jquery.sparkline.min.js"></script>
 	
 	<!-- Owl JavaScript -->
-	<script src="admin_assets/vendors/bower_components/owl.carousel/dist/owl.carousel.min.js"></script>
+	<script src="admin/admin_assets/vendors/bower_components/owl.carousel/dist/owl.carousel.min.js"></script>
 	
 	<!-- Toast JavaScript -->
-	<script src="admin_assets/vendors/bower_components/jquery-toast-plugin/dist/jquery.toast.min.js"></script>
+	<script src="admin/admin_assets/vendors/bower_components/jquery-toast-plugin/dist/jquery.toast.min.js"></script>
 	
 	<!-- EChartJS JavaScript -->
-	<script src="admin_assets/vendors/bower_components/echarts/dist/echarts-en.min.js"></script>
-	<script src="admin_assets/vendors/echarts-liquidfill.min.js"></script>
+	<script src="admin/admin_assets/vendors/bower_components/echarts/dist/echarts-en.min.js"></script>
+	<script src="admin/admin_assets/vendors/echarts-liquidfill.min.js"></script>
 	
 	<!-- Switchery JavaScript -->
-	<script src="admin_assets/vendors/bower_components/switchery/dist/switchery.min.js"></script>
+	<script src="admin/admin_assets/vendors/bower_components/switchery/dist/switchery.min.js"></script>
 	
 	<!-- Init JavaScript -->
-	<script src="admin_assets/dist/js/init.js"></script>
-	<script src="admin_assets/dist/js/dashboard-data.js"></script>
+	<script src="admin/admin_assets/dist/js/init.js"></script>
+	<script src="admin/admin_assets/dist/js/dashboard-data.js"></script>
 </body>
 
 </html>
